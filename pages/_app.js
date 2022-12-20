dd353f5e4d39c73cf85b5a787f6d3b9fe199abdb
Tùng Đường <>
2022-12-20 15:07:11 +0700

    Add Vercel Tracking

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,14 @@ import Head from 'next/head'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+import AppAnalytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import ProgressBar from 'react-scroll-progress-bar'
 import ScrollTop from '@/components/ScrollTop'
 import { SessionProvider } from 'next-auth/react'
 import { Provider } from '@lyket/react'
+import { Analytics } from '@vercel/analytics/react'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -60,9 +61,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
           <meta content="width=device-width, initial-scale=1" name="viewport" />
         </Head>
         {isDevelopment && isSocket && <ClientReload />}
-        <Analytics />
+        <AppAnalytics />
         <LayoutWrapper>
           <Component {...pageProps} />
+          <Analytics />
         </LayoutWrapper>
       </ThemeProvider>
     </SessionProvider>
